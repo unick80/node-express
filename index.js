62a27abe92d479576175fc35349424e54f2569e1
Nick Pisarev <zagis-net@mail.ru>
2020-04-20 00:29:48 +0300

    Подключение express-handlebars

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+app.engine('hbs',hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.get('/', (req,res)=>{
     res.status(200);
