1aadb4f218570d505a0903207d340ff05a09c275
Nick Pisarev <zagis-net@mail.ru>
2020-04-20 14:13:08 +0300

    Настройка Layout, подключение materializecss

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req,res)=>{
 });
 
 app.get('/about', (req,res)=>{
-    res.render('aboit');});
+    res.render('about');});
 
 const PORT = process.env.PORT||3000;
 app.listen(PORT, () =>{
