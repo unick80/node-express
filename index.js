7466f3efbdb6e3476932911f309252a6307acbbd
Nick Pisarev <zagis-net@mail.ru>
2020-04-20 14:32:52 +0300

    Дабавили навигацию

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.engine('hbs',hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(express.static('public'));
+
 app.get('/', (req,res)=>{
     res.render('index');
 });
