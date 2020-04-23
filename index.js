ddeef126d87aa6abcb1fb847c0640f2e2edab570
Nick Pisarev <zagis-net@mail.ru>
2020-04-23 13:55:49 +0300

    Переписываем модель, allowInsecurePrototypeAccess

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} =require('@handlebars/allow-prototype-access');
 const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
@@ -11,9 +13,10 @@ const app = express();
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
-    extname: 'hbs'
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(handlebars)
 });
-app.engine('hbs',hbs.engine);
+app.engine('hbs',hbs.engine,{});
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,7 @@ const Course = require('../models/course');
 const router = Router();
 
 router.get('/', async (req,res) => {
-    const courses = await Course.getAll();
+    const courses = await Course.find();
     res.render('courses', {
         title: "Курсы",
         isCourse: true,
@@ -15,7 +15,7 @@ router.get('/:id/edit', async (req,res) =>{
     if(!req.query.allow){
         return res.redirect('/');
     }
-    const course = await Course.getById(req.params.id);
+    const course = await Course.findById(req.params.id);
     res.render('course-edit', {
         title: `Редактировать ${course.title}`,
         course
@@ -23,12 +23,14 @@ router.get('/:id/edit', async (req,res) =>{
 });
 
 router.post('/edit', async (req,res) => {
-    await Course.update(req.body);
+    const {id} = req.body;
+    delete req.body.id;
+    await Course.findByIdAndUpdate(id,req.body);
     res.redirect('/courses');
 });
 
 router.get('/:id', async (req,res) =>{
-    const course = await Course.getById(req.params.id);
+    const course = await Course.findById(req.params.id);
     res.render('course',{
         layout: 'empty',
         title: `Курс ${course.title}`,
