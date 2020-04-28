824f127ec3df48b5441c0591854d4b301b094a65
Nick Pisarev <zagis-net@mail.ru>
2020-04-28 13:48:38 +0300

    Сохранение сессии

--- index.js
+++ index.js
@@ -25,16 +25,6 @@ app.engine('hbs',hbs.engine,{});
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use( async (req, res, next) => {
-    try {
-        const user = await User.findById('5ea28609c53c330d5841ca52');
-        req.user = user;
-        next();
-    } catch (e) {
-        console.log(e);
-    }
-});
-
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(session({
@@ -57,15 +47,15 @@ async function start() {
     try {
         const url = 'mongodb+srv://nick:RiucoB9iZXh5VX0r@cluster0-8oit1.mongodb.net/shop';
         await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-        const candidate = await User.findOne();
-        if (!candidate){
-            const user = new User({
-                email: 'zagis-net@mail.ru',
-                name: 'nick',
-                cart: {items: []}
-            });
-            await user.save();
-        }
+        //const candidate = await User.findOne();
+        //if (!candidate){
+        //    const user = new User({
+        //        email: 'zagis-net@mail.ru',
+        //        name: 'nick',
+        //        cart: {items: []}
+        //    });
+        //    await user.save();
+        //}
         app.listen(PORT, () =>{
             console.log(`Server is running on port ${PORT}`);
         });
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const User = require('../models/user');
 const router = Router();
 
 router.get('/login', async (req, res) => {
@@ -15,7 +16,13 @@ router.get('/logout', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
+    const user = await User.findById('5ea28609c53c330d5841ca52');
+    req.session.user = user;
     req.session.isAuthenticated = true;
-    res.redirect('/');
+    req.session.save(err => {
+        if (err) throw err;
+        res.redirect('/');
+    });
+    
 });
 module.exports = router;
