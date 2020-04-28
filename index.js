babaf03a3352266a0ef3e7b4adc6e4f3fe6c31de
Nick Pisarev <zagis-net@mail.ru>
2020-04-28 15:12:03 +0300

    Сессия в базе данных

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 const {allowInsecurePrototypeAccess} =require('@handlebars/allow-prototype-access');
 const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
@@ -14,12 +15,16 @@ const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 const varMiddleware = require('./middleware/variables');
 
+const MONGODB_URI = 'mongodb+srv://nick:RiucoB9iZXh5VX0r@cluster0-8oit1.mongodb.net/shop';
 const app = express();
-
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
     handlebars: allowInsecurePrototypeAccess(handlebars)
+});
+const store = new MongoStore({
+    collection: 'session',
+    uri: MONGODB_URI
 });
 app.engine('hbs',hbs.engine,{});
 app.set('view engine', 'hbs');
@@ -30,7 +35,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(session({
     secret: 'some secret value',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store
 }));
 app.use(varMiddleware);
 
@@ -45,17 +51,7 @@ const PORT = process.env.PORT||3000;
 
 async function start() {
     try {
-        const url = 'mongodb+srv://nick:RiucoB9iZXh5VX0r@cluster0-8oit1.mongodb.net/shop';
-        await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-        //const candidate = await User.findOne();
-        //if (!candidate){
-        //    const user = new User({
-        //        email: 'zagis-net@mail.ru',
-        //        name: 'nick',
-        //        cart: {items: []}
-        //    });
-        //    await user.save();
-        //}
+        await mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
         app.listen(PORT, () =>{
             console.log(`Server is running on port ${PORT}`);
         });
